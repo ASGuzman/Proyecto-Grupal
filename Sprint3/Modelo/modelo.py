906ae10924aa33d3605a899693977d688bd15de1
ASGuzman <aylusolguzmanhoyos@gmail.com>
2024-01-15 17:56:27 -0300

    New Commit

--- Sprint3/Modelo/modelo.py
+++ Sprint3/Modelo/modelo.py
@@ -47,34 +47,17 @@ def procesar_texto(texto):
 # Procesar texto
 data['reseña_procesada'] = data['text'].apply(procesar_texto)
 
-# Crear un vectorizador TF-IDF para procesar las reseñas
-tfidf_vectorizer = TfidfVectorizer()
-tfidf_matrix = tfidf_vectorizer.fit_transform(data['reseña_procesada'])
+# Streamlit App
+st.title("Recomendaciones de Restaurantes")
 
-# Calcular similitud de coseno entre las reseñas
-cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
-
-# Crear un modelo de vecinos más cercanos (KNN)
-knn_model = NearestNeighbors(n_neighbors=5, metric='cosine')
-knn_model.fit(tfidf_matrix)
-
-# Reducir tamaño de datos después de TF-IDF
-del data['text']
-del tfidf_vectorizer
-
-# Reducir tamaño de datos después de similitud de coseno
-del tfidf_matrix
+ciudad = st.selectbox("Seleccione el nombre de la ciudad:", data['city'].unique())
+min_estrellas = st.slider("Seleccione la cantidad mínima de estrellas:", 0.0, 5.0, 0.0, 0.5)
 
 # Función para obtener recomendaciones
 def obtener_recomendaciones(nombre_ciudad, min_estrellas):
-    # Filtrar por ciudad y por cantidad mínima de estrellas
-    data_ciudad = data[(data['city'] == nombre_ciudad) & (data['avg_rating'] >= min_estrellas)]
-
-    if data_ciudad.empty:
-        return "No hay restaurantes que cumplan con los criterios."
-
-    # Calcular TF-IDF para las reseñas de la ciudad filtrada
-    tfidf_ciudad = tfidf_vectorizer.transform(data_ciudad['reseña_procesada'])
+    # Crear un vectorizador TF-IDF para procesar las reseñas
+    tfidf_vectorizer = TfidfVectorizer()
+    tfidf_ciudad = tfidf_vectorizer.fit_transform(data_ciudad['reseña_procesada'])
 
     # Encontrar vecinos más cercanos basados en similitud de coseno
     _, indices = knn_model.kneighbors(tfidf_ciudad)
@@ -83,12 +66,6 @@ def obtener_recomendaciones(nombre_ciudad, min_estrellas):
     top3_recomendaciones = data.iloc[indices[0]].head(3)['name'].tolist()
 
     return top3_recomendaciones
-
-# Streamlit App
-st.title("Recomendaciones de Restaurantes")
-
-ciudad = st.selectbox("Seleccione el nombre de la ciudad:", data['city'].unique())
-min_estrellas = st.slider("Seleccione la cantidad mínima de estrellas:", 0.0, 5.0, 0.0, 0.5)
 
 if st.button("Obtener Recomendaciones"):
     recomendaciones = obtener_recomendaciones(ciudad, min_estrellas)
